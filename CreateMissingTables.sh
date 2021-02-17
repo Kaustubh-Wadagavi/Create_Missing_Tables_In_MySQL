63a7f52fe5be69c81da20b0952f247e073c55720
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-02-17 15:16:58 +0530

    Script to create the missing tables schema

--- CreateMissingTables.sh
+++ CreateMissingTables.sh
@@ -5,7 +5,7 @@ IGNORED_TABLES_STRING="";
 while read PRESENT_TABLES
 do
  IGNORED_TABLES_STRING+=" --ignore-table=os_mysql.${PRESENT_TABLES}"
-done<os_tables.csv
+done < os_tables.csv
 
 mysqldump -uos_tester -psecrete --single-transaction --skip-lock-tables --no-data --routines --complete-insert ${IGNORED_TABLES_STRING} os_mysql > os_extra_tables.sql
 
