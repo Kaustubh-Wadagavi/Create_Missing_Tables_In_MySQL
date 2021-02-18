accc07bf9c16c05a2058dcc12be68cc777f7cff9
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-02-18 13:50:59 +0530

    Added concatnation command

--- CreateMissingTables.sh
+++ CreateMissingTables.sh
@@ -5,8 +5,12 @@ IGNORED_TABLES_STRING="";
 while read PRESENT_TABLES
 do
  IGNORED_TABLES_STRING+=" --ignore-table=os_mysql.${PRESENT_TABLES}"
-done < <ENTER PATH TO CSV FILE>
+done < os_tables.csv
 
-mysqldump -uos_tester -psecrete --single-transaction --skip-lock-tables --no-data --routines --complete-insert ${IGNORED_TABLES_STRING} os_mysql > <ENTER FULL PATH OF SPECIFIC SUITE WITH FILE NAME>
+mysqldump -uos_tester -psecrete --single-transaction --skip-lock-tables --no-data --routines --complete-insert ${IGNORED_TABLES_STRING} os_mysql > os_extra_tables.sql
 
-sed -i 's#<parameter name="schemaDump" value="/schema.sql"></parameter>.*#<parameter name="schemaDump" value="/schema.sql,/<ENTER-SPECIFIC-SUITE-DIRECTORY-NAME>/os_extra_tables.sql"></parameter>#' <ENTER FULL PATH TO SPECIFIC SUITE.XML FILE>
+cat os_extra_tables.sql os_fresh.sql > temp.sql && mv temp.sql DP_extra_tables_and_data_dump.sql
+
+
+
+
