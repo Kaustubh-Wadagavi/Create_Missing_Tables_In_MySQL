025b74c38d29fb6c29620c225d64bdbf1a781c41
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-02-18 12:10:19 +0530

    Added command to concat string

--- CreateMissingTables.sh
+++ CreateMissingTables.sh
@@ -5,8 +5,8 @@ IGNORED_TABLES_STRING="";
 while read PRESENT_TABLES
 do
  IGNORED_TABLES_STRING+=" --ignore-table=os_mysql.${PRESENT_TABLES}"
-done < os_tables.csv
+done < <ENTER PATH TO CSV FILE>
 
-mysqldump -uos_tester -psecrete --single-transaction --skip-lock-tables --no-data --routines --complete-insert ${IGNORED_TABLES_STRING} os_mysql > /home/krishagni/Desktop/os-api-tests/src/test/resources/distribution_protocols/os_extra_tables.sql
+mysqldump -uos_tester -psecrete --single-transaction --skip-lock-tables --no-data --routines --complete-insert ${IGNORED_TABLES_STRING} os_mysql > <ENTER FULL PATH OF SPECIFIC SUITE WITH FILE NAME>
 
-sed -i 's#<parameter name="schemaDump" value="/schema.sql"></parameter>.*#<parameter name="schemaDump" value="/schema.sql,<Enter Directory Name>/os_extra_tables.sql"></parameter>#' <Enter full path to specific suite.xml file>
+sed -i 's#<parameter name="schemaDump" value="/schema.sql"></parameter>.*#<parameter name="schemaDump" value="/schema.sql,/<ENTER-SPECIFIC-SUITE-DIRECTORY-NAME>/os_extra_tables.sql"></parameter>#' <ENTER FULL PATH TO SPECIFIC SUITE.XML FILE>
